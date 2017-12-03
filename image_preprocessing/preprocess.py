c350376c60c4d7976644b71db9a5f33db6a4fd7e
esdream <conquerfu@gmail.com>
2017-12-03 15:12:46 +0800

    preprocess-WIP

--- image_preprocessing/preprocess.py
+++ image_preprocessing/preprocess.py
@@ -33,3 +33,27 @@ def preprocess_for_train(image, height, width, bbox):
         image = tf.image.convert_image_dtype(image, dtype=tf.float32)
 
     # 随机截取图像，减小需要关注的物体大小对图像识别算法的影响
+    bbox_begin, bbox_size, _ = tf.image.sample_distorted_bounding_box(tf.shape(image), bounding_boxes=bbox)
+    distorted_image = tf.slice(image, bbox_begin, bbox_size)
+    
+    # 将随机截取的图像调整为神经网络输入层的大小。大小调整的算法是随机选择的
+    distorted_image = tf.image.resize_images(distorted_image, height, width, method=np.random.randint(4))
+    # 随机左右翻转图像
+    distorted_image = tf.image.random_flip_left_right(distorted_image)
+    # 使用一种随机的顺序调整图像色彩
+    distorted_image = distort_color(distorted_image, np,random.randint(2))
+    
+    return distorted_image
+
+if(__name__ == '__main__'):
+    image_raw_data = tf.gfile.FastGFile('./image/cat.jpg', 'rb').read()
+
+    with tf.Session() as sess:
+        img_data = tf.image.decode_jpeg(image_raw_data)
+
+        for i in range(6):
+            # 将图像尺寸调整为128, 128
+            result = preprocess_for_train(img_data, 128, 128, None)
+            plt.imshow(result.eval())
+            plt.show()
+            
